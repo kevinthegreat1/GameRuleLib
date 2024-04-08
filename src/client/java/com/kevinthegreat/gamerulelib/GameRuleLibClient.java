6f9983390e821558564124b29449f8bd74d76ea7
Kevinthegreat <92656833+kevinthegreat1@users.noreply.github.com>
2024-04-08 10:34:19 -0400

    Mod setup

---
+++ src/client/java/com/kevinthegreat/gamerulelib/GameRuleLibClient.java
@@ -0,0 +1,9 @@
+package com.kevinthegreat.gamerulelib;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GameRuleLibClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
