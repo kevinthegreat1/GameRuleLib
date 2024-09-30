3a7a770017401ba3514e7423ceba543b6c01eae7
Kevinthegreat <92656833+kevinthegreat1@users.noreply.github.com>
2024-09-29 21:49:02 -0400

    Update to 1.21.1

--- src/main/java/com/kevinthegreat/gamerulelib/impl/GameRuleUpdateS2CPacket.java
+++ src/main/java/com/kevinthegreat/gamerulelib/impl/GameRuleUpdateS2CPacket.java
@@ -8,7 +8,7 @@ import net.minecraft.util.Identifier;
 import net.minecraft.world.GameRules;
 
 public record GameRuleUpdateS2CPacket(String name, String value) implements CustomPayload {
-    public static final CustomPayload.Id<GameRuleUpdateS2CPacket> PACKET_ID = new CustomPayload.Id<>(new Identifier(GameRuleLib.MOD_ID, "game_rule_update"));
+    public static final CustomPayload.Id<GameRuleUpdateS2CPacket> PACKET_ID = new CustomPayload.Id<>(Identifier.of(GameRuleLib.MOD_ID, "game_rule_update"));
     public static final PacketCodec<RegistryByteBuf, GameRuleUpdateS2CPacket> PACKET_CODEC = PacketCodec.tuple(PacketCodecs.STRING, GameRuleUpdateS2CPacket::name, PacketCodecs.STRING, GameRuleUpdateS2CPacket::value, GameRuleUpdateS2CPacket::new);
 
     public GameRuleUpdateS2CPacket(String name, GameRules.Rule<?> rule) {
